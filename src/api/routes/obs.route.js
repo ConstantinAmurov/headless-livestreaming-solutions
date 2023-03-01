c5ff07e9e2195e02e5ed213f6af9f5f7905e14f1
CostelMD <amurov.constantin@gmail.com>
2023-03-01 15:20:06 +0100

    Added custom ToggleWithTransitionScene request

--- src/api/routes/obs.route.js
+++ src/api/routes/obs.route.js
@@ -14,21 +14,64 @@ router.get("/:requestType", async (req, res) => {
   const { requestType } = req.params;
   try {
     const obsResponse = await obs.call(requestType);
-    res.send(obsResponse);
-  } catch (error) {
-    res.send(error);
-  }
-});
-router.post("/:requestType", async (req, res) => {
-  const { requestType } = req.params;
-  try {
-    const obsRes = await obs.call(requestType, { ...req.body });
-    res.send(obsRes);
+    res.send(
+      `${requestType} request made succesfully.\n Response: ${JSON.stringify(
+        obsResponse
+      )}`
+    );
   } catch (error) {
     res.send(
       `Failed to send request: CODE: ${error.code}, MESSAGE: ${error.message}`
     );
   }
 });
+
+router.post("/ToggleWithTransitionScene", async (req, res) => {
+  // Execute a transition sequence to a different scene with a specific transition.
+  try {
+    const { sceneName, transitionName } = req.body;
+
+    await obs.callBatch([
+      {
+        requestType: "SetCurrentPreviewScene",
+        requestData: { sceneName },
+      },
+      {
+        requestType: "SetCurrentSceneTransition",
+        requestData: { transitionName },
+      },
+      {
+        requestType: "Sleep",
+        requestData: { sleepMillis: 100 },
+      },
+      {
+        requestType: "TriggerStudioModeTransition",
+      },
+    ]);
+    res.send(`ToggleWithTransitionScene request made succesfully.\n}`);
+  } catch (error) {
+    res.send(
+      `Failed to send request: CODE: ${error.code}, MESSAGE: ${error.message}`
+    );
+  }
+});
+
+router.post("/:requestType", async (req, res) => {
+  console.log('General Reqeust');
+  const { requestType } = req.params;
+  try {
+    const obsResponse = await obs.call(requestType, { ...req.body });
+    res.send(
+      `${requestType} request made succesfully.\n Response: ${JSON.stringify(
+        obsResponse
+      )}`
+    );
+  } catch (error) {
+    res.send(
+      `Failed to send request: CODE: ${error.code}, MESSAGE: ${error.message}`
+    );
+  }
+});
+
 
 module.exports = router;
